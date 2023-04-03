28cb3d6febbe9ec15e97f54994d15155023436bf
tgoodall3 <Tylergoodall3@gmail.com>
2023-04-02 20:51:14 -0400

    Final

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,7 @@ var usersExist = false;
 var userFullName = "";
 var _db = "";
 var _userProfInfo = {};
+// var lists = [];
 
 function barlistener(){
     $(".bars").click(function(e){
@@ -53,6 +54,123 @@ function initURLlisteners(){
     changeRoute();
 }
 
+function viewRecipe(){
+  console.log("View recipe");
+  $("#app").html(` 
+  <div class="wrapper">
+  <div class="detail-recipe">
+    <div class="header">
+      <h1>supreme pizza</h1>
+    </div>
+    <div class="food-image">
+      <img src="images/recipe-pizza.png" alt="" />
+    </div>
+    <div class="food-description">
+      <div class="head">
+        <h1>Description:</h1>
+        <span
+          >Make pizza night super duper out of this world with homemade
+          pizza. This recipe is supreme with vegetables and two types of
+          meat. Yum!</span
+        >
+      </div>
+      <div class="info">
+        <span>Total Time:</span>
+        <span>1h 24min</span>
+        <span>Servings:</span>
+        <span>4 servings</span>
+      </div>
+    </div>
+  </div>
+  <div class="ingredients">
+    <div class="header">
+      <h1>Ingredients:</h1>
+    </div>
+    <div class="list">
+      <span>1/4 batch pizza dough</span>
+      <span>2 tablespoons Last-Minute Pizza Sauce</span>
+      <span>10 slices pepperoni</span>
+      <span>1 cup cooked and crumbled Italian sausage </span>
+      <span>2 large mushrooms, sliced </span>
+      <span>1/4 bell pepper, sliced</span>
+      <span>1 tablespoon sliced black olives</span>
+      <span>1 cup shredded mozzarella cheese</span>
+    </div>
+  </div>
+  <div class="instructions">
+    <div class="header">
+      <h1>Instructions:</h1>
+    </div>
+    <div class="list">
+      <span
+        >1. Preheat the oven to 475°. Spray pizza pan with nonstick cooking
+        or line a baking sheet with parchment paper.</span
+      >
+      <span
+        >2. Flatten dough into a thin round and place on the pizza pan.
+      </span>
+      <span>3. Spread pizza sauce over the dough. </span>
+      <span
+        >4. Layer the toppings over the dough in the order listed .
+      </span>
+      <span
+        >5. Bake for 8 to 10 minutes or until the crust is crisp and the
+        cheese melted and lightly browned.</span
+      >
+    </div>
+  </div>
+  <a href="editRecipe.html"><button>Edit Recipe</button></a>
+</div>
+  `);
+}
+
+function editRecipe(){
+  // e.preventDefault();
+  console.log("edit recipe");
+  $("#app").html(` 
+  <div class="recipe">
+        <div class="create">
+            <form class="recipe">
+              <label for="fName">Add Recipe Image</label>
+              <input id="fName" type="text" name="First Name" />
+              <label for="LName">Recipe Name</label>
+              <input id="lName" type="text" name="Last Name" />
+              <label for="email">Recipe Description</label>
+              <input id="email" type="text" name="Email" />
+              <label for="improve">Recipe Total Time</label>
+              <input id="improve" type="text" name="Improve" />
+              <label for="concerns">Recipe Serving Size?</label>
+              <input id="concerns" type="text" name="concerns" />
+              </form>
+      
+              <form class="Ingredient">
+              <label for="me">Ingredient #1</label>
+              <input id="me" type="text" name="me" />
+              <label for="valuable">Ingredient #2</label>
+              <input id="valuable" type="text" name="valuable" />
+              <label for="reccomendations">Ingredient #3</label>
+              <input id="reccomend" type="text" name="reccomend" />
+          </form>
+      
+              <form class="Instruction">
+                <label for="Anything">Instruction #1</label>
+                <input id="anything" type="text" name="anything" />
+                <label for="message">Instruction #2</label>
+                <input id="message" type="text" name="message" />
+                <label for="message">Instruction #3</label>
+                <input id="message" type="text" name="message" />
+              </form>
+      
+              <div class="CreateSub">
+                <input id="submit" type="submit" value="Submit" />
+              </div>
+            </form>
+          </div>
+    </div>
+  `);
+
+}
+
 
 function initFirebase(){
   firebase.auth().onAuthStateChanged((user)=> {
@@ -94,17 +212,21 @@ function initFirebase(){
 
 function addRecipe(){
   let newRecipeName = $("#recipeName").val();
-  let newRecipeObj = {
+    let newRecipeObj = {
     name: newRecipeName,
     recipeItems: [],
-  };
+    };
+    
 
+    console.log("work");
 
   _userProfInfo.lists.push(newRecipeObj);
   updateUserInfo(_userProfInfo);
   loadRecipe();
   $("#recipeName").val();
-  }
+  } 
+
+
 
 function updateUserInfo(userObj){
   let id = firebase.auth().currentUser.uid;
